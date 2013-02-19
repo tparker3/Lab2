b548dcf4716b4bcb83ec58ea686a219b966fe4be
TParks <tparker3@unca.edu>
2013-02-19 16:14:51 -0500

    changed content in both index and app. Figured out appropiate jquery coding

--- ctwitter-master/app.js
+++ ctwitter-master/app.js
@@ -2,11 +2,17 @@ var main = function () {
     console.log("hello world");
   
     var twitter = new ctwitter.CTwitter();
-    twitter.stream("statuses/filter", { lang:"en", track:["New Media"] }, function (stream) {
+    twitter.stream("statuses/filter", { lang:"en", track:["Social Media"] }, function (stream) {
         stream.on("data", function (tweet) {
-              $("#tweets").append("<img src='"+tweet.profile_image_url+"' />");
-              console.log(tweet.profile_image_url);
-
+              //$("#tweets").append("<img src='"+tweet.profile_image_url+"' />");
+             var tweet = $("<p>"+tweet.text+"</p>") 
+            tweet.hide();
+              $("#tweets").append(tweet);
+              console.log(tweet.text);
+               tweet.fadeIn();
+                tweet.fadeOut(10000);
+                  tweet.slideDown();
+                
         });
     });
 }
